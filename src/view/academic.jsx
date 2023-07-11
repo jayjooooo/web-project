9ced384098cf0c37b37739bcc0f0cbbcd2f94560
Jay Joo <jayjooofx@gmail.com>
2023-07-11 18:38:22 +0700

    update academic

--- src/view/academic.jsx
+++ src/view/academic.jsx
@@ -1,3 +1,5 @@
+import logo_pnj from ".././view/4.png";
+
 function academic() {
   return (
     <div className="Poppins">
@@ -46,6 +48,127 @@ function academic() {
                 info lebih lanjut kunjungi penerimaan.pnj.ac.id
               </p>
             </div>
+          </div>
+        </div>
+
+        <div className="ml-24 mr-96">
+          <div className="text-left font-bold text-xl">
+            <h1>Program Studi</h1>
+          </div>
+          <div className="bg-blue-600 px-6 py-3 mt-4">
+            <h1 className="text-left text-xl underline">
+              SARJANA TERAPAN TEKNIK INFORMATIKA
+            </h1>
+            <p className="text-justify">
+              Program studi sarjana terapan teknik informatika merupakan program
+              studi yang berfokus pada pengembangan teknologi rekayasa perngkat
+              lunak. ranah dari prodi ini adalah berfokus pada pengembangan
+              seperti aplikasi, sistem operasi dan website maupun data engineer.
+              lulusan dari prodi teknik informatika PNJ tersebar di berbagai
+              sektor seperti Software Engineer, Website Developer, maupun QA IT.
+            </p>
+          </div>
+          <div className="bg-blue-600 px-6 py-3 my-9">
+            <h1 className="text-left text-xl underline">
+              SARJANA TERAPAN TEKNIK MULTIMEDIA JARINGAN{" "}
+            </h1>
+            <p className="text-justify">
+              SARJANA TERAPAN TEKNIK MULTIMEDIA JARINGAN Program studi sarjana
+              terapan teknik multimedia jaringan berfokus pada pengembangan
+              infrastruktur jaringan atau interkonektivtas antar komputer. ranah
+              dari prodi ini terbagi menjadi beberapa peminatan seperti embeded
+              system, machine learning, cyber security, dan lain lain. lulusan
+              dari prodi ini tersebar diberbagai sektor seperti cyber security,
+              Telekomunikasi, teknisi sistem komputer,dsb
+            </p>
+          </div>
+          <div className="bg-blue-600 px-6 py-3">
+            <h1 className="text-left text-xl underline">
+              SARJANA TERAPAN TEKNIK MULTIMEDIA DIGITAL{" "}
+            </h1>
+            <p className="text-justify">
+              Program studi sarjana terapan teknik multimedia digital berfokus
+              pada pengembangan Multimedia seperti animasi, user interface, user
+              experience, game, sampai pada pengembangan teknologi metaverse
+              seperti avatar dsb. lulusan dari prodi ini tersebar di berbagai
+              sektor seperti web designer, game developer, animation engineer,
+              dsb.
+            </p>
+          </div>
+
+          <div className="text-left font-bold text-xl mt-10">
+            <h1>Capaian Lulusan</h1>
+          </div>
+
+          <div className="mt-4 text-xl">
+            <p className="text-justify">
+              lulusan Teknik informatika dan Komputer untuk program studi
+              sarjana terapan, mereka dibekali sertifikasi profesi yang sesuai
+              dengan KKNI level IV yang setara dengan lulusan sarjana akademik.
+              tidak hanya itu, lulusan juga dibekali dengan sertifikat tes
+              bahasa inggris TOEIC yang menjadi syarat kelulusan.
+            </p>
+          </div>
+          <div className="mt-9 text-xl">
+            <p className="text-justify">
+              masa studi sarjana terapan adalah setara dengan 144 SKS yang dapat
+              selsaikan dalam waktu 4 tahun atau selama lamanya adalah 5 tahun.
+              Mahasiswa lulusan TIK PNJ yang sah secara sidang yudisium berhak
+              menyadang gelar Sarjana terapan ilmu komputer atau S.Tr.Kom
+            </p>
+          </div>
+
+          <div className="text-left font-bold text-xl mt-10">
+            <h1>Kehidupan Mahasiswa</h1>
+          </div>
+
+          <div className="mt-4 text-xl">
+            <p className="text-justify">
+              Selama berkuliah, mahasiswa tidak hanya dibekali pembelajaran
+              dalam kampus tetapi PNJ juga menyediakan wadah bagi mahasiswa
+              untuk bersosialisasi dan berorganisasi antar sesama mahasiswa.
+              mahasiswa TIK juga diberikan kesempatan untuk mengajar, magang,
+              dan studi independent melalui program MBKM yang dimulai pada
+              semester 5
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-zinc-800 py-16 text-white">
+        <div className="flex justify-between items-center mx-28">
+          <div className="flex justify-between items-center">
+            <div className="mx-3">
+              <img src={logo_pnj} alt="" className="w-28" />
+            </div>
+            <div className="mx-3">
+              <h1 className="text-left text-xl font-bold">
+                Department of <br /> Informatics and Computer <br />
+                engineering
+              </h1>
+            </div>
+          </div>
+
+          <div className="text-left">
+            <div>
+              <h1 className="text-xl font-bold">Akademik</h1>
+            </div>
+            <ul>
+              <li>Sarjana Terapan Teknik Informatika</li>
+              <li>Sarjana Terapan Teknik Multimedia Digital</li>
+              <li>Sarjana Terapan Teknik Multimedia Digital dan Jaringan</li>
+              <li>D1 Teknik Komputer dan Jaringan</li>
+            </ul>
+          </div>
+
+          <div className="text-left">
+            <div className="text-xl font-bold">Akses Cepat</div>
+            <ul>
+              <li>WEweb Utama PNJ</li>
+              <li>Siak Mahasiswa</li>
+              <li>E-Learning</li>
+              <li>Gudang Ilmu TIK</li>
+            </ul>
           </div>
         </div>
       </div>
