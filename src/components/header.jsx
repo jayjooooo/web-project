dbebc0e3c142ddc1aad1257a675137298df832f9
Randy Ananda <88238381+RandyAnanda11@users.noreply.github.com>
2023-07-12 18:34:09 +0700

    rombak header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,44 +1,44 @@
-import Home from ".././view/home";
-import about from ".././view/about";
-import contact from ".././view/contact";
-import akademik from ".././view/academic";
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import logo_pnj from ".././view/4.png";
 
 function header() {
   return (
-    <div className="bg-blue-950 text-white Poppins">
-      <div className="flex justify-between items-center top-0 left-0 right-0 py-4">
-        <div className="ml-10">
-          <img src={logo_pnj} alt="" className="w-20 rounded-full" />
-        </div>
-        <a className="text-2xl mr-48 text-left">
-          Department of <br /> Informatics and Computer engineering
-        </a>
-        <div className="inline-block text-xl">
-          <a
-            className="mx-4 hover:text-blue-700 transition-colors duration-300"
-            href="/"
-          >
-            Home
+    <div className="relative z-50">
+      <div className="bg-blue-500/30 backdrop-blur-md text-white Poppins fixed top-0 left-0 right-0  overflow-hidden  ">
+        <div className="flex justify-between items-center top-0 left-0 right-0 py-1 ">
+          <div className="ml-10">
+            <img src={logo_pnj} alt="" className="w-20 rounded-full" />
+          </div>
+          <a href="#" className="text-l text-left p-5">
+            Departemen
+            <br /> Teknik Informatika dan Komputer
           </a>
-          <a
-            className="mx-4 hover:text-blue-700 transition-colors duration-300"
-            href="/akademik"
-          >
-            Academic
-          </a>
-          <a
-            className="mx-4 hover:text-blue-700 transition-colors duration-300"
-            href="/about"
-          >
-            About Us
-          </a>
-          <a
-            className="mx-4 hover:text-blue-700 transition-colors duration-300"
-            href="/contact"
-          >
-            Contact
-          </a>
+          <div className="inline-block text-l ml-auto ">
+            <a
+              className="mx-4  hover:bg-white rounded-xl p-3 transition-all hover:text-blue-600 hover:ring-4 ring-blue cursor-pointer"
+              href="/"
+            >
+              Home
+            </a>
+            <a
+              className="mx-4 hover:bg-white rounded-xl p-3 transition-all hover:text-blue-600 hover:ring-4 ring-blue cursor-pointer"
+              href="/Academic"
+            >
+              Academic
+            </a>
+            <a
+              className="mx-4  hover:bg-white rounded-xl p-3 transition-all hover:text-blue-600 hover:ring-4 ring-blue cursor-pointer"
+              href="/about"
+            >
+              About Us
+            </a>
+            <a
+              className="mx-4 hover:bg-white rounded-xl p-3 transition-all hover:text-blue-600 hover:ring-4 ring-blue cursor-pointer"
+              href="/contact"
+            >
+              Contact
+            </a>
+          </div>
         </div>
       </div>
     </div>
