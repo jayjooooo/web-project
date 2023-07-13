cee9990ad6675a60f7a782104ab4b10635f8f3be
Randy Ananda <88238381+RandyAnanda11@users.noreply.github.com>
2023-07-13 14:22:09 +0700

    Update home.jsx

--- src/view/home.jsx
+++ src/view/home.jsx
@@ -150,70 +150,7 @@ function home() {
         </div>
       </div>
 
-      <div className="Footer text-white">
-        <div>
-          <div className="bg-zinc-800 pt-5 mx-auto ">
-            <div className="flex justify-between items-center mx-20 mb-10">
-              <div className="flex justify-between items-center">
-                <div className="ml-[-20px]">
-                  <img src={logo_pnj} alt="" className="w-28" />
-                </div>
-                <div className="mx-3">
-                  <h1 className="text-left text-l font-based">
-                    Departemen <br /> Teknik Informatika dan <br />
-                    Komputer
-                  </h1>
-                </div>
-              </div>
-
-              <div className="text-left">
-                <div>
-                  <h1 className="text-l font-based mb-2 border-b-2 border-white">
-                    Akademik
-                  </h1>
-                </div>
-                <ul className="font-based">
-                  <li className="hover:underline transition-all">
-                    Sarjana Terapan Teknik Informatika
-                  </li>
-                  <li>Sarjana Terapan Teknik Multimedia Digital</li>
-                  <li className="hover:underline transition-all">
-                    Sarjana Terapan Teknik Multimedia Digital dan Jaringan
-                  </li>
-                  <li className="hover:underline transition-all">
-                    D1 Teknik Komputer dan Jaringan
-                  </li>
-                </ul>
-              </div>
-
-              <div className="text-left">
-                <div className="text-l fon-based mb-2 border-b-2 border-white">
-                  Akses Cepat
-                </div>
-                <ul className="font-based">
-                  <li className="hover:underline transition-all">
-                    Web Utama PNJ
-                  </li>
-                  <li className="hover:underline transition-all">
-                    Siak Mahasiswa
-                  </li>
-                  <li className="hover:underline transition-all">E-Learning</li>
-                  <li className="hover:underline transition-all">
-                    Gudang Ilmu TIK
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              <hr className="text-slate-800" />
-              <p className="p-2">
-                Developed By Randy & Fajar, Calon Junior Web Devlopment 2022 -
-                2026
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      
     </div>
   );
 }
